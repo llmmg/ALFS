690c9668c244e855d0cc6c4dede5d4c2bba7fe10
lancelot <lancelotlancelot94@gmail.com>
2017-04-21 15:11:16 +0200

    added test condition to avoid creating a file system over the current os

--- res/createPartition.sh
+++ res/createPartition.sh
@@ -1,7 +1,14 @@
 #Create a new partition on the given drive
-#TODO: verify that the given dev isnt the os drive
 
-echo "Creating partition on"$1
-mkfs -v -t ext4 /dev/$1
+echo "Creating partition on "$1
 
+#verify that the given dev isnt the os drive
+#get where os is mounted
+MY_PATH=$(mount | grep " on / " | cut -d " " -f 1)
 
+if [[ $MY_PATH == $1 ]]; then
+    echo "WARNING!!!! given path is the directory of current installed os"
+else
+    echo "$1 is correct"
+    mkfs.ext4 /dev/$1
+fi
